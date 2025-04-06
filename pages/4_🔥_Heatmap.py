d071a2d5b28f8783d47f2d3e24b177c822e80ed4
Bekzhan Bekenov <bekzhanbekenov2003@gmail.com>
2025-04-06 21:38:31 +0500

    3

--- pages/4_🔥_Heatmap.py
+++ pages/4_🔥_Heatmap.py
@@ -1,44 +1,83 @@
-import zipfile
-import os
-import geopandas as gpd
 import streamlit as st
 import leafmap.foliumap as leafmap
+from io import BytesIO
+import ee
+import streamlit as st
+import geemap.foliumap as geemap
+import geopandas as gpd
+import matplotlib.pyplot as plt
+import zipfile
+import tempfile
+import os
 
-# Путь к архиву
-archive_path = "C:/Users/bekzh/Desktop/дипломка/agroclim_N_C_E.zip"
+st.set_page_config(layout="wide")
 
-# Распаковываем архив
-with zipfile.ZipFile(archive_path, 'r') as zip_ref:
-    # Список всех файлов в архиве
-    zip_ref.printdir()
+st.sidebar.info(
+    """
+    - Web App URL: <https://streamlit.gishub.org>
+    - GitHub repository: <https://github.com/giswqs/streamlit-geospatial>
+    """
+)
 
-    # Извлекаем все файлы в папку "extracted_files"
-    zip_ref.extractall("extracted_files")
+st.sidebar.title("Contact")
+# Upload a zipped shapefile
+    uploaded_shp_file = st.sidebar.file_uploader("Upload a Zipped Shapefile", type=["zip"])
 
-# Проверяем содержимое извлеченной папки
-extracted_files = os.listdir("extracted_files")
-st.write("Извлеченные файлы:", extracted_files)
+    if uploaded_shp_file is not None:
+        # Extract the zip file
+        with tempfile.TemporaryDirectory() as tmpdir:
+            with zipfile.ZipFile(uploaded_shp_file, 'r') as zip_ref:
+                zip_ref.extractall(tmpdir)
 
-# Ищем шейп-файлы (.shp)
-shapefiles = [f for f in extracted_files if f.endswith('.shp')]
+            # Find the shapefile within the extracted files
+            shapefile_path = None
+            for root, dirs, files in os.walk(tmpdir):
+                for file in files:
+                    if file.endswith(".shp"):
+                        shapefile_path = os.path.join(root, file)
+                        break
 
-# Если шейп-файл найден, загружаем его
-if shapefiles:
-    shapefile_path = os.path.join("extracted_files", shapefiles[0])  # Берем первый найденный шейп-файл
-    st.write(f"Загружаем шейп-файл: {shapefile_path}")
+            if shapefile_path:
+                # Read the shapefile into a GeoDataFrame
+                gdf = gpd.read_file(shapefile_path)
 
-    # Загружаем шейп-файл с помощью geopandas
-    try:
-        gdf = gpd.read_file(shapefile_path)
-        st.write(gdf)  # Отображаем DataFrame
+                # Create the plot
+                fig, ax = plt.subplots()
+                gdf.plot(ax=ax)
+                plt.xticks(rotation=90, fontsize=7)
+                plt.yticks(fontsize=7)
 
-        # Создаем карту с шейп-файлом
-        st.title("Map with Shapefile Data")
+                # Save the plot to a BytesIO object
+                buf = BytesIO()
+                plt.savefig(buf, format='png')
+                buf.seek(0)
 
+                # Display the plot in Streamlit
+                with row2_col1:
+                    st.image(buf, caption='Geopandas Plot')
+            else:
+                st.error("Shapefile (.shp) not found in the uploaded zip file.")
+
+            if not gdf.empty:
+                roi = geemap.geopandas_to_ee(gdf)
+st.sidebar.info(
+    """
+    Qiusheng Wu at [wetlands.io](https://wetlands.io) | [GitHub](https://github.com/giswqs) | [Twitter](https://twitter.com/giswqs) | [YouTube](https://youtube.com/@giswqs) | [LinkedIn](https://www.linkedin.com/in/giswqs)
+    """
+)
+
+st.title("Heatmap")
+
+with st.expander("See source code"):
+    with st.echo():
+        filepath = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv"
         m = leafmap.Map(center=[40, -100], zoom=4)
-        m.add_gdf(gdf, layer_name="Shapefile Layer")
-        m.to_streamlit(height=700)
-    except Exception as e:
-        st.error(f"Ошибка при загрузке шейп-файла: {e}")
-else:
-    st.error("Шейп-файлы не найдены в извлеченных данных.")
+        m.add_heatmap(
+            filepath,
+            latitude="latitude",
+            longitude="longitude",
+            value="pop_max",
+            name="Heat map",
+            radius=20,
+        )
+m.to_streamlit(height=700)
