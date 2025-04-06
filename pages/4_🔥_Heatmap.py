09ae58490049fb86db0a89a6eefeb6bf44e8db75
Bekzhan Bekenov <bekzhanbekenov2003@gmail.com>
2025-04-06 22:53:52 +0500

    5

--- pages/4_🔥_Heatmap.py
+++ pages/4_🔥_Heatmap.py
@@ -32,7 +32,7 @@ m.to_streamlit(center=[48.0196, 66.9237], zoom=5, height=600)
 
 # Отображение карты по умолчанию
 st.subheader("Default Interactive Map")
-st_folium(m, width=700, key="default_map")
+st_folium(m, width=700)
 
 # Функция загрузки архива с шейп-файлами
 uploaded_shp_file = st.sidebar.file_uploader("Upload a Zipped Shapefile", type=["zip"])
