7f9ecfbb10fec0bf658a6e6878b859beff389d15
Bekzhan Bekenov <bekzhanbekenov2003@gmail.com>
2025-04-06 22:18:38 +0500

    3

--- pages/4_🔥_Heatmap.py
+++ pages/4_🔥_Heatmap.py
@@ -28,7 +28,6 @@ st.title("Interactive Map with Heatmap")
 
 # Изначально показываем карту с центром на Казахстане
 m = leafmap.Map(center=[48.0196, 66.9237], zoom=5)
-m.to_streamlit(center=[48.0196, 66.9237], zoom=5, height=600)
 
 # Отображение карты по умолчанию
 st.subheader("Default Interactive Map")
@@ -68,3 +67,5 @@ if uploaded_shp_file is not None:
             st_folium(m, width=700)
         else:
             st.error("Шейп-файл (.shp) не найден в загруженном архиве.")
+
+m.to_streamlit(center=[48.0196, 66.9237], zoom=5, height=600)
