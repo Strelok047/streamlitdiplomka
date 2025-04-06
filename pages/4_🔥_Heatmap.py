ae5b94ab3dddb004bee46e020d52c59fb3125cc5
Bekzhan Bekenov <bekzhanbekenov2003@gmail.com>
2025-04-06 21:25:23 +0500

    3

--- pages/4_🔥_Heatmap.py
+++ pages/4_🔥_Heatmap.py
@@ -5,7 +5,7 @@ import streamlit as st
 import leafmap.foliumap as leafmap
 
 # Путь к архиву
-archive_path = "C:/Users/bekzh/Desktop/дипломка/agroclim_N_C_E.zip"
+archive_path = ""C:\Users\bekzh\Desktop\дипломка\agroclim_N_C_E.zip
 
 # Распаковываем архив
 with zipfile.ZipFile(archive_path, 'r') as zip_ref:
@@ -15,36 +15,27 @@ with zipfile.ZipFile(archive_path, 'r') as zip_ref:
     # Извлекаем все файлы
     zip_ref.extractall("extracted_files")  # Здесь файлы будут извлечены в папку "extracted_files"
 
-# Проверяем содержимое извлеченной папки
-extracted_files = os.listdir("extracted_files")
-st.write("Извлеченные файлы:", extracted_files)
-
 # Предположим, что один из файлов - это CSV
-# Замените 'your_data.csv' на фактическое имя CSV файла
-csv_file_path = os.path.join("extracted_files", "agroclim_data.csv")
+csv_file_path = os.path.join("extracted_files", "your_data.csv")
 
-# Проверяем, существует ли CSV файл
-if os.path.exists(csv_file_path):
-    # Загружаем CSV в DataFrame
-    df = pd.read_csv(csv_file_path)
+# Загружаем CSV в DataFrame
+df = pd.read_csv(csv_file_path)
 
-    # Отображаем данные в Streamlit
-    st.write(df)
+# Отображаем данные в Streamlit
+st.write(df)
 
-    # Создаем карту с тепловой картой
-    st.title("Heatmap")
+# Создаем карту с тепловой картой
+st.title("Heatmap")
 
-    with st.expander("See source code"):
-        with st.echo():
-            m = leafmap.Map(center=[40, -100], zoom=4)
-            m.add_heatmap(
-                csv_file_path,
-                latitude="latitude",
-                longitude="longitude",
-                value="pop_max",
-                name="Heat map",
-                radius=20,
-            )
-    m.to_streamlit(height=700)
-else:
-    st.error("CSV файл не найден в извлеченных данных.")
+with st.expander("See source code"):
+    with st.echo():
+        m = leafmap.Map(center=[40, -100], zoom=4)
+        m.add_heatmap(
+            csv_file_path,
+            latitude="latitude",
+            longitude="longitude",
+            value="pop_max",
+            name="Heat map",
+            radius=20,
+        )
+m.to_streamlit(height=700)
